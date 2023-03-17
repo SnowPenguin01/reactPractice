8dd1feea60336186d02e0cfbd4dbcd18172e3baa
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-03-17 11:22:00 +0300

    Update FormCalcCopy.jsx

--- src/components/UI/FormCalcCopy.jsx
+++ src/components/UI/FormCalcCopy.jsx
@@ -1,85 +1,107 @@
 import React from "react";
+import axios from 'axios';
+import SwitchButton from './SwitchButton';
 
-class FormCalcCopy extends React.Component {
+const apiURL = "http://new.energobank.su/local/ajax/calculator.php?PID=15373";
 
-    constructor(props){
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            sum: []
-        };
+const FormCalcCopy = () => {
+
+    const [sum, setSum] = React.useState(null);
+    const [year, setYear] = React.useState(null);
+    const [rangeval, setRangeval] = React.useState(5000000);
+    const [rangeval2, setRangeval2] = React.useState(42);
+
+    function calc(){
+        let percent = 16.5 / 12 / 100;
+        let isn = Math.pow(1 + percent, rangeval2);
+        let rr = Math.ceil(rangeval * percent * (isn/(isn - 1))) + ' ₽/мес.';
+        document.querySelector('.content-title').textContent = rr;
     }
+    
+    React.useEffect(() => {
+        axios.get(apiURL).then((response) => {
+            setSum(response.data);
+            setYear(response.data);
+        });
+      }, [sum , year]);
 
-    componentDidMount(){
-        fetch('http://new.energobank.su/local/ajax/calculator.php?PID=15373')
-            .then(response => response.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        sum: result.sum
-                    });
-                },
-
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error: true
-                    }); 
-                }
-            )
-    }
+    if (!sum || !year) return null;
 
 
-    render(){
-
-        const { error, isLoaded, sum } = this.state;
-        if (error) {
-            return <div>Ошибка: {error.message}</div>;
-          } else if (!isLoaded) {
-            return <div>Загрузка...</div>;
-          } else {
-            return(
-                <div className="calc-block__control">
-                    <form action="" className="form-group">
-                        <div className="button-switch">
-                            <p className="button-switch__text button-switch__text_view">С обеспечением</p>
-                            <input type="checkbox" className="button-switch__checkbox" />
-                            <p className="button-switch__text button-switch__text_view">Без обеспечения</p>
-                        </div>
-                        <div className="form-input-group">
-                            <p className="form-text">Сумма кредита</p>
-                            <input className="form-input" type="text" id="amount" name="amount_money" placeholder="5 000 000₽" />
-                            <input className="form-range sumFormAttr" type="range" id="amountRange" name="amountRange_money"  value="" />
-                        </div>
-                        <div className="under-form-text">
-                            {sum.map(summa => (
-                                <p className="form-text credit-number__min" key={summa.id}>
-                                    {summa.min_value}
-                                </p>
-                            ))}
-                            <p className="form-text credit-number__max"></p>
-                        </div>
-                        <div className="application-text">
-                            <p>Для обсуждения персональных условий <a class="application-link" href="index.html">оставьте заявку</a></p>
-                        </div>
-                        <div className="form-input-group">
-                            <p className="form-text">Срок кредита</p>
-                            <input className="form-input" type="text" id="amountYear" name="amount_year" placeholder="2,5 года" />
-                            <input className="form-range yearFormAttr" type="range" id="amountYears" name="amount_year" value="" />
-                        </div>
-                        <div className="under-form-text">
-                            <p className="form-text credit-data__min"></p>
-                            <p className="form-text credit-data__max"></p>
-                        </div>
-                    </form>
-                </div>
-            )
-        }
-
+    return(
+        <div className="calc-block__control">
+            <form action="" className="form-group">
+                <SwitchButton />
+                <div className="form-input-group">
+                    <p className="form-text">Сумма кредита</p>
+                    <input 
+                        className="form-input"
+                        type="text" id="amount" 
+                        name="amount_money" 
+                        placeholder="5 000 000₽" 
+                        defaultValue={rangeval}
+                    />
+                    <input
+                        className="form-range sumFormAttr" 
+                        type="range" 
+                        id="amountRange" 
+                        name="amountRange_money" 
+                        defaultValue={sum.sum.max_value / 2} 
+                        min={sum.sum.min_value} 
+                        max={sum.sum.max_value} 
+                        onChange={(event) => {
+                            let sumNumberRange = document.querySelector('#amountRange').value;
+                            let showSumNumber = new Intl.NumberFormat('ru-RU', {
+                                style: "currency",
+                                currency: 'RUB'
+                            }).format(sumNumberRange);
         
-    }
+                            document.querySelector('#amount').value = showSumNumber;
+                            setRangeval(event.target.value);
+                            calc();
+                        }} 
+                    />
+                </div>
+                <div className="under-form-text">
+                    <p className="form-text credit-number__min">{sum.sum.min_value + ' ₽'}</p>
+                    <p className="form-text credit-number__max">{sum.sum.max_value + ' ₽'}</p>
+                </div>
+                <div className="application-text">
+                    <p>Для обсуждения персональных условий <a class="application-link" href="index.html">оставьте заявку</a></p>
+                </div>
+                <div className="form-input-group">
+                    <p className="form-text">Срок кредита</p>
+                    <input 
+                        className="form-input" 
+                        type="text" 
+                        id="amountYear" 
+                        name="amount_year" 
+                        placeholder="2,5 года" 
+                        defaultValue={rangeval2}
+                    />
+                    <input 
+                        className="form-range yearFormAttr" 
+                        type="range" 
+                        id="amountYears"
+                        name="amount_year" 
+                        defaultValue={sum.term.max_value / 2}
+                        min={sum.term.min_value} 
+                        max={sum.term.max_value} 
+                        onChange={(event) => {
+                            let resultYears = document.querySelector('#amountYears').value + ' мес.';
+                            document.querySelector('#amountYear').value = resultYears;
+                            setRangeval2(event.target.value);
+                            calc();
+                        }}
+                    />
+                </div>
+                <div className="under-form-text">
+                    <p className="form-text credit-data__min">{sum.term.min_value + ' мес.'}</p>
+                    <p className="form-text credit-data__max">{sum.term.max_value / 12 + ' лет'}</p>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default FormCalcCopy;

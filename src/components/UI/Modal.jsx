219a5d832213d069e7a1cf1dd3b7ff6974b48df9
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-03-17 11:23:03 +0300

    Create Modal.jsx

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Modal = ({active, setActive}) => {
+
+    
+
+    return(
+        <div className={active ? "modal active" : "modal"} onClick={() => {
+            setActive(false)
+        }}>
+            <div className="modal__content" onClick={e => e.stopPropagation()}>
+                <p className="modal__text">Ваша заявка в размере рублей успешно отправлена! <br />
+                С обеспечением:  <br />
+                Акция:  <br />
+                На срок кредита: <br /></p>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;

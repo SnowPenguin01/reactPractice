9b8effdb6fe6055dac90bdb53067143b9cd5f099
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-04-04 11:38:29 +0300

    Update FormCalcCopy.jsx

--- src/components/UI/FormCaclCopy/FormCalcCopy.jsx
+++ src/components/UI/FormCaclCopy/FormCalcCopy.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios';
 import SwitchButton from '../SwitchButton/SwitchButton';
+import '../FormCaclCopy/FormCalc.scss';
 
 const FormCalcCopy = () => {
 

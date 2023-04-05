6b303e10910aa78b757196ceca38cce437efc59f
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-04-05 10:27:52 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,29 @@
 import axios from "axios";
 
-const apiForCalc = axios.get(process.env.REACT_APP_API_KEY);
+// const baseURL = process.env.REACT_APP_API_KEY; // http://new.energobank.su/local/ajax
 
+// const api = () => {
+//   const instance = axios.create({
+//     baseURL: baseURL,
+//   });
+// };
+
+// const apiFuncs = {
+//   getCalc: async (params) => await api().get(`/calculator.php`, { params }),
+// };
+
+// export default apiFuncs;
+
+
+
+
+
+
+// старый пример
+// import axios from "axios";
+const apiForCalc = axios.get(process.env.REACT_APP_API_KEY);
 export default apiForCalc;
+
 
 // TODO: пример как лучше делать
 /*

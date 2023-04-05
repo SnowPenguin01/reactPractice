ce299cc034ed4930c7edb52eca3843ec7b619b7c
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-04-05 11:40:22 +0300

    Update SwitchButton.jsx

--- src/components/UI/SwitchButton/SwitchButton.jsx
+++ src/components/UI/SwitchButton/SwitchButton.jsx
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import '../SwitchButton/switchButton.scss';
 
 const SwitchButton = () => {
-  // TODO: использовать useState
+    // TODO: использовать useState
 
-  return (
-    <div className="button-switch">
-      <p className="button-switch__text button-switch__text_view">
-        С обеспечением
-      </p>
-      <input
-        type="checkbox"
-        className="button-switch__checkbox"
-        defaultChecked
-        onClick={() => {
-          let with1 = "Да";
-          let withOut = "Нет";
+    const [switchCheck, setSwitchCheck] = useState(false)
 
-          if (document.querySelector(".button-switch__checkbox").checked) {
-            document.querySelector(".button-switch__checkbox").value = withOut;
-          } else {
-            document.querySelector(".button-switch__checkbox").value = with1;
-          }
-        }}
-      />
-      <p className="button-switch__text button-switch__text_view">
-        Без обеспечения
-      </p>
-    </div>
-  );
-};
+    const handleOnChange = () => {
+      setSwitchCheck(!switchCheck)
+      switchValue();
+    };
+
+    function switchValue(){
+      if (switchCheck){
+        document.querySelector(".button-switch__checkbox").value = "Да"
+      } else {
+        document.querySelector(".button-switch__checkbox").value = "Нет"
+      }
+    }
+
+    return (
+        <div className="button-switch">
+          <p className="button-switch__text button-switch__text_view">
+            С обеспечением
+          </p>
+          <input
+            type="checkbox"
+            className="button-switch__checkbox"
+            defaultChecked={switchCheck}
+            onChange={handleOnChange}
+          />
+          <p className="button-switch__text button-switch__text_view">
+            Без обеспечения
+          </p>
+        </div>
+      );
+    };
 
 export default SwitchButton;

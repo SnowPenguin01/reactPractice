a276595aca16be16c12af77c3e668a9aeec7b756
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-04-19 12:47:11 +0300

    Update Modal.jsx

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import '../Modal/modal.scss';
-
-const Modal = ({active, setActive}) => {
-
-    
+ 
+const Modal = ({active, setActive, formik}) => {
 
     return(
         <div className={active ? "modal active" : "modal"} onClick={() => {
             setActive(false)
         }}>
             <div className="modal__content" onClick={e => e.stopPropagation()}>
-                <p className="modal__text">Ваша заявка в размере рублей успешно отправлена! <br />
-                С обеспечением:  <br />
-                Акция:  <br />
-                На срок кредита: <br /></p>
+                <p className="modal__text">Ваша заявка в размере {formik.values.sum} рублей успешно отправлена! <br />
+                С обеспечением: {formik.values.switchVal} <br />
+                Акция 16,5%: {formik.values.promotionVal}  <br />
+                На срок кредита: {formik.values.year} <br /></p>
             </div>
         </div>
     )

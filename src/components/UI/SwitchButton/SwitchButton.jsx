d7c1d1d1f531758216e6283c3f385d1b79a265ce
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-04-19 12:46:11 +0300

    Update SwitchButton.jsx

--- src/components/UI/SwitchButton/SwitchButton.jsx
+++ src/components/UI/SwitchButton/SwitchButton.jsx
@@ -1,41 +1,24 @@
-import React, { useState } from "react";
-import "../SwitchButton/switchButton.scss";
+import React from "react";
+import '../SwitchButton/switchButton.scss';
 
-const SwitchButton = () => {
-  // TODO: использовать useState
-  // сделано
+const SwitchButton = ({formik, handleOnChangeSwitchBtn}) => {
 
-  const [switchCheck, setSwitchCheck] = useState(false);
-
-  const handleOnChange = () => {
-    setSwitchCheck(!switchCheck);
-    // switchValue();
-  };
-
-  // function switchValue(){
-  //   if (switchCheck){
-  //     document.querySelector(".button-switch__checkbox").value = "Да"
-  //   } else {
-  //     document.querySelector(".button-switch__checkbox").value = "Нет"
-  //   }
-  // }
-
-  return (
-    <div className="button-switch">
-      <p className="button-switch__text button-switch__text_view">
-        С обеспечением
-      </p>
-      <input
-        type="checkbox"
-        className="button-switch__checkbox"
-        defaultChecked={switchCheck}
-        onChange={handleOnChange}
-      />
-      <p className="button-switch__text button-switch__text_view">
-        Без обеспечения
-      </p>
-    </div>
-  );
-};
+    return (
+        <div className="button-switch">
+          <p className="button-switch__text button-switch__text_view">
+            С обеспечением
+          </p>
+          <input
+            type="checkbox"
+            className="button-switch__checkbox"
+            defaultChecked={formik.initialValues.switchCheckValue}
+            onChange={handleOnChangeSwitchBtn}
+          />
+          <p className="button-switch__text button-switch__text_view">
+            Без обеспечения
+          </p>
+        </div>
+      );
+    };
 
 export default SwitchButton;
